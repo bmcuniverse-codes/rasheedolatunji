0dd1716f64639765e5ea2d5d3709663ac6901f69
bmcuniverse-codes <tourwithasta@gmail.com>
2026-06-09 03:05:55 -0700

    API and Response Updated

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
